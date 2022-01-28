8170a9606cdac18ad4092866c1b5ec7e8b29be7f
Andre Luiz <andretechsouza@outlook.com>
2022-01-28 20:43:51 -0300

    Remoção de classes e ajuste nos imports

--- src/main/java/br/com/microservico/imc/config/IMC.java
+++ src/main/java/br/com/microservico/imc/config/IMC.java
@@ -3,12 +3,8 @@ package br.com.microservico.imc.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "br.com.microservico.imc.config")
-@ComponentScan(value = "br.com.microservico.imc")
-@EnableFeignClients
 public class IMC {
 
     public static void main(String[] args) {
